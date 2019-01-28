f8462aafaf3c6733f72f25d10c7ecde319045cce
Tim Williams <timwilliams.tx@gmail.com>
2019-01-28 23:28:41 +0000

    add ws server and connected to client

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,12 @@ class App extends Component {
       // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
     }, 3000);
+
+  let socket = new WebSocket('ws://localhost:3001')
+    socket.onopen = () => {
+      console.log('Connected to Server')
+}
+
   }
 
 
